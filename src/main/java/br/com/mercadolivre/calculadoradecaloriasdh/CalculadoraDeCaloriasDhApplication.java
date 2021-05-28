712788999f9dcca27494a73ced165c0d68edfd65
matheusmeli <matheus.agomes@mercadolivre.com>
2021-05-28 11:21:52 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/mercadolivre/calculadoradecaloriasdh/CalculadoraDeCaloriasDhApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.calculadoradecaloriasdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraDeCaloriasDhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraDeCaloriasDhApplication.class, args);
+	}
+
+}
